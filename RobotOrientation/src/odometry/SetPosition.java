74f04a9a1a3f14c5872b3749a078c52c2d7dba2a
Spikes-2212 <spikes.no2212@gmail.com>
2019-01-26 20:22:05 +0200

    added SetPosition command

---
+++ RobotOrientation/src/odometry/SetPosition.java
@@ -0,0 +1,23 @@
+package odometry;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+public class SetPosition extends InstantCommand {
+	private double x, y, yaw;
+	private OdometryHandler handler;
+	private Runnable resetSensors;
+
+	public SetPosition(double x, double y, double yaw, Runnable resetSensors, OdometryHandler handler) {
+		this.x = x;
+		this.y = y;
+		this.yaw = yaw;
+		this.resetSensors = resetSensors;
+		this.handler = handler;
+	}
+
+	@Override
+	protected void initialize() {
+		resetSensors.run();
+		handler.setPosition(x, y, yaw);
+	}
+}
